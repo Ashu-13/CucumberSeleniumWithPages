f60f96a867486c69240fb7f9a24a1715ff0d943a
ASHUTOSH <ASHUTOSH@LAPTOP-VQCRCEQ5>
2023-12-03 21:14:47 +0530

    second commit

--- PSA/src/test/java/com/stepDefs/practice/LoginStepDef.java
+++ PSA/src/test/java/com/stepDefs/practice/LoginStepDef.java
@@ -1,13 +1,16 @@
 package com.stepDefs.practice;
 
 import io.cucumber.java.en.Given;
+import utils.ConfigReader;
 
+
+import commons.Base;
 public class LoginStepDef {
 	
 	
 	@Given("user launches retailstat application")
 	public void i_want_to_write_a_step_def() {
-		
+		ConfigReader.readProperty("qaURL");
 		System.out.println("=========="+"-------------");
 	}
 
--- PSA/src/test/java/testRunner/TestRunner.java
+++ PSA/src/test/java/testRunner/TestRunner.java
@@ -11,5 +11,6 @@ import io.cucumber.junit.CucumberOptions;
 	
 		)
 public class TestRunner {
+	
 
 }
